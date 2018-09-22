ce6b5a9cc6aad3a5bb5e4abf71cf44b5595900a5
Nathan Oliver <nathan.oliver@iotecglobal.com>
2018-09-22 14:05:06 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,50 @@
+import os
 from setuptools import setup, find_packages
 
-from test import PyTest
-from version import __version__
+
+about = {
+    'here': os.path.abspath(os.path.dirname(__file__))
+}
+
+with open(os.path.join(about['here'], 'version.py')) as f:
+    exec (f.read(), about)
+
+with open(os.path.join(about['here'], 'test', '__init__.py')) as f:
+    exec (f.read(), about)
+
+with open(os.path.join(about['here'], 'README.md')) as f:
+    about['readme'] = f.read()
+
 
 setup(
+    # available in PKG-INFO
     name='beeswax_wrapper',
-    version=__version__,
-    description='Python2 Wrapper for the Beeswax API',
+    version=about['__version__'],
+    description='Python Wrapper for the Beeswax API',
+    url='https://github.com/iotgdev/beeswax_wrapper/',
     author='iotec',
     author_email='dev@dsp.io',
-    url='https://github.com/iotgdev/beeswax_wrapper/',
-    download_url='https://github.com/iotgdev/beeswax_wrapper/archive/{}.tar.gz'.format(__version__),
+    license='MIT',
+    download_url='https://github.com/iotgdev/beeswax_wrapper/archive/{}.tar.gz'.format(about['__version__']),
+    long_description=about['readme'],
+    platforms=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+
+    # Package Properties
     packages=find_packages(include=['beeswax_wrapper', 'beeswax_wrapper.*']),
-    data_files=[],
+    include_package_data=True,
     test_suite='test',
     setup_requires=['pytest-runner'],
     tests_require=['mock>=2.0.0', 'pytest'],
-    cmdclass={'pytest': PyTest},
-    scripts=['beeswax_wrapper/credentials/beeswax_credentials'],
-    include_package_data=True,
+    cmdclass={'pytest': about['PyTest']},
+    scripts=['bin/beeswax_credentials'],
     install_requires=[
         'keyrings.alt>=3.1',
         'keyring>=8.2.1',
